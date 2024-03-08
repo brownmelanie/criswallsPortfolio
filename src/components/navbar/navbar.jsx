20d3d8fd2a375201c7e3cabc5e144ed2654eb436
Melanie Brown <melaniebrown.bm@gmail.com>
2024-03-07 21:58:38 -0300

    Arreglo navbar falta transparent

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,41 +3,51 @@ import { Bars2Icon, XMarkIcon } from '@heroicons/react/24/solid'
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
+
     let Links =[
         {name:"MUSIC VIDEOS", link:"/"},
         {name:"COVER ARTS", link:"/"},
         {name:"ABOUT ME", link:"/"},
         {name:"CONTACT", link:"/"}
       ];
-let [open, setOpen] =useState(false);
+
+
+    let [open, setOpen] = useState(false);
+
+    const toggleButton = () => {
+        setOpen(!open)
+    }
 
     return (
-        <div className={`absolute border-solid border border-x-transparent border-t-transparent border-b-neutral-700 w-full top-0 left-0 z-10`}>
-            <div className='items-center justify-between bg-transparent md:px-7 px-7 h-13 py-6'>
-    {/*LOGO HERE*/}
+        <nav className='bg-stone-800'>
+            <div className='flex items-center justify-between bg-transparent md:px-7 px-7 h-13 py-6'>
                 <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
-                    <h2 className='text-neutral-700 font-Helvetica'>CRISWALLS</h2>
+                    <h2 className=' text-stone-50 font-Helvetica'>CRISWALLS</h2>
                 </div>
-    {/*ICON HERE*/}
-                <div onClick={()=>setOpen(!open)} className='absolute right-14 top-6 cursor-pointer w-7 h-7 py-0'>
-                    <div className='absolute right-20 top-0.5'>
-                        <Link className='text-neutral-700 font-Helvetica font-bold'>WORKS</Link>
-                    </div>
-                    {
-                        open ? <XMarkIcon className='text-neutral-700'/> : <Bars2Icon className='text-neutral-700 font-bold'/>
+                <div className=' flex flex-row items-center gap-x-5'>
+                    <Link to="#" className=' text-stone-50 cursor-pointer hover:text-stone-400 duration-300'>W O R K S</Link>
+                    { open 
+                    ?
+                    <button className='' onClick={toggleButton}>
+                        <XMarkIcon className=' text-stone-50 w-6 h-auto' />
+                    </button>
+                    :
+                    <button className='' onClick={toggleButton}>
+                        <Bars2Icon className=' text-stone-50  w-6 h-auto' /> 
+                    </button>
                     }
+                    
                 </div>
-    {/*LINKS HERE*/}
-                <ul  className={`z-10 md:z-10 absolute left-0 top-20 bg-transparent p-4 pr-8 text-right transition-transform duration-500 ease-in-out ${open ? 'md:right-80 right-36 transform md:translate-x-72 translate-x-full' : 'md:right-80 right-36 transform md:translate-x-full translate-x-28'}`}>
-                {
-                    Links.map((link) => (
-                        <li key={link.href} className='md:ml-8 md:my-5 my-5 font-Helvetica font-semibold'>
-                            <a href={link.link}className='text-neutral-800 hover:text-neutral-500 duration-500'>{link.name}</a>
-                        </li>))
-                }
+                <ul className={`bg-stone-800 absolute flex flex-col justify-end z-10 top-20 p-4 px-9 text-right rounded-b-md duration-150 ease-in ${open ? 'right-0' : ' right-[-199px]'}`}>
+                    {
+                        Links.map((link) => (
+                            <li key={link.href} className='md:ml-8 md:my-5 my-5 font-Helvetica font-semibold'>
+                                <a href={link.link}className='text-neutral-800 hover:text-neutral-500 duration-300 text-stone-50'>{link.name}</a>
+                            </li>))
+                    }
                 </ul>
             </div>
-        </div>
+        </nav>
     );
 };
 
